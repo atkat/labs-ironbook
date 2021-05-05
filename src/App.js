0f5b65df039918f2c6ce3db948f84deefc7a72e0
Katarina Orlovska <k.orlovska@posteo.de>
2021-05-05 18:41:27 +0200

    it's not pretty yet but it all works :

--- src/App.js
+++ src/App.js
@@ -1,43 +1,105 @@
-import logo from './logo.svg';
+
 import './App.css';
 import users from "./users";
 import React, { Component } from 'react'
 
-
 class App extends Component {
-
-render () {
+  
   state = {
-    users: users
+    users: users,
+    search: "",
+    studentChecked: false,
+    teacherChecked: false,
+    campus: ""
   }
 
-  const mappedUsers = this.state.users.map( user => (
-    <tr key={user.firstName}>
+  
+  handleChange = event => {
+    const name = event.target.name;
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    console.log(value);
+    this.setState({
+      [name]: value
+    })
+  }      
+
+  render () {
+    let regex = new RegExp(this.state.search, "gi")
+
+    const filteredUsers = this.state.users.filter(user => {
+      if (this.state.studentChecked) return user.role === "student";
+      if (this.state.teacherChecked) return user.role === "teacher";
+      if (this.state.search) return  `${user.firstName}${user.lastName}`.match(regex);
+      if (this.state.campus) return user.campus === this.state.campus
+      else return this.state.users
+  })
+
+  const mappedUsers = filteredUsers.map( user => (
+    <tr >
+      <td>{user.firstName}</td>
       <td>{user.lastName}</td>
-      <td>{user.popularity}</td>
-      <td><button onClick={() => this.removeuser(user.id)}>Remove user</button></td>
+      <td>{user.campus}</td>
+      <td>{user.role}</td>
+      <td>{user.linkedin && <a href="{user.linkedin}">LinkedIn</a>}</td>
     </tr>
    ))
 
   return (
     <>
-      <button onClick={this.addRandomuser}>Add new user</button>
-      <button onClick={this.sortAlphabetically}>Sort alphabetically</button>
-      <button onClick={this.sortPopularity}>Sort by popularity</button>
-      <table>
-        <thead>
-            <tr>
-              <td>First Name</td>
-              <td>Last Name</td>
-              <td>Campus</td>
-              <td>Role</td>
-              <td>Links</td>
-            </tr>
-        </thead>
-        <tbody>
-          {mappedUsers}
-        </tbody>
-      </table>
+    <h1>IronBook</h1>
+     <div class="input">
+       <form action="" class="inputForm">
+         <input 
+            type="text"
+            placeholder="type here..."
+            name="search"
+            id="search"
+            value={this.state.search}
+            onChange={this.handleChange}
+         />
+         <br/>
+        <label htmlFor="">Student</label>
+        <input
+            type="checkbox"
+            name="studentChecked"
+            id="role"
+            checked={this.state.studentChecked}
+            onChange={this.handleChange}
+          />
+        <label htmlFor="">Teacher</label>
+           <input
+            type="checkbox"
+            name="teacherChecked"
+            id="role"
+            checked={this.state.teacherChecked}
+            onChange={this.handleChange}
+          />
+          <select name="campus" onChange={this.handleChange} value={this.state.campus}>
+            <option value="">--Please choose an option--</option>
+            <option value="Berlin">Berlin</option>
+            <option value="Amsterdam">Amsterdam</option>
+            <option value="Paris">Paris</option>
+          </select>
+       </form> 
+       <br/>
+     </div>
+
+      <div>
+        <table>
+          <thead>
+              <tr>
+                <td>First Name</td>
+                <td>Last Name</td>
+                <td>Campus</td>
+                <td>Role</td>
+                <td>Links</td>
+              </tr>
+          </thead>
+          <tbody>
+            {mappedUsers}
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
