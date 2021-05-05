2da6fa62e79d8cde54b2b068a88eff59899d574d
Katarina Orlovska <k.orlovska@posteo.de>
2021-05-05 15:37:44 +0200

    back to filter :coffee:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import users from "./users";
+import React, { Component } from 'react'
 
-function App() {
+
+class App extends Component {
+
+render () {
+  state = {
+    users: users
+  }
+
+  const mappedUsers = this.state.users.map( user => (
+    <tr key={user.firstName}>
+      <td>{user.lastName}</td>
+      <td>{user.popularity}</td>
+      <td><button onClick={() => this.removeuser(user.id)}>Remove user</button></td>
+    </tr>
+   ))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button onClick={this.addRandomuser}>Add new user</button>
+      <button onClick={this.sortAlphabetically}>Sort alphabetically</button>
+      <button onClick={this.sortPopularity}>Sort by popularity</button>
+      <table>
+        <thead>
+            <tr>
+              <td>First Name</td>
+              <td>Last Name</td>
+              <td>Campus</td>
+              <td>Role</td>
+              <td>Links</td>
+            </tr>
+        </thead>
+        <tbody>
+          {mappedUsers}
+        </tbody>
+      </table>
+    </>
+  )
+}
 }
 
 export default App;
